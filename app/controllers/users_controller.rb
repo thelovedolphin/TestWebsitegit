5e09b2ae599668d98b55366b13ec36e9d79494d5
thelovedolphin <mstaylormoon@gmail.com>
2021-06-23 01:10:19 -0600

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def create
-  end
+    @user = User.create(params.require(:user).permit(:username,        
+    :password))
+    session[:user_id] = @user.id
+    redirect_to '/welcome'
+ end
+ def new
+  @user = User.new
+ end
 end
